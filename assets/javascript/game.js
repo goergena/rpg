6c6520cfdceb58203bb3fbf3853e58dab9bee637
Ali <ali@Alis-MBP.tremontplacelofts.local>
2018-02-19 18:29:06 -0500

    attacks work, choosing new opponent works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,39 +2,159 @@ $(document).ready(function(){
 
 //set all ids to variables
 
+
     var twilight = $("#twilight");
     var celestia = $("#celestia");
     var discord = $("#discord");
     var nightmare = $("#nightmare");
 
-    var ponyStats = {
-        //nest functions
-    }
 
+  
     isHeroChosen = false;
     isChallengerChosen = false;
-    isEnemyDefeated = false;
+    isChallengerDefeated = false;
+    allEnemiesDefeated = false;
+    isHeroDefeated = false;
     var hero = "";
+    var heroId = "";
+    var challenger = "";
+    var challengerId = "";
+    var heroHealth = 100;
+    var challengerHealth = 100;
+    var heroAttack = 0;
+    var challengerAttack = 0;
+    var currentAttack = 0;
+   // var heroHealthNum = 0; 
+   // var challengerHealthNum = 0; 
+
+
+    function attackTime () {
+        if (heroId==="twilight"){
+            heroAttack = 5;
+        } else if (heroId==="celestia"){
+            heroAttack = 20;
+        } else if (heroId==="discord"){
+            heroAttack = 15;
+        } else if (heroId==="nightmare"){
+            heroAttack = 10;
+        } 
+        if (challengerId==="twilight"){
+            challengerAttack = 5;
+        } else if (challengerId==="celestia"){
+            challengerAttack = 20;
+        } else if (challengerId==="discord"){
+            challengerAttack = 15;
+        } else if (challengerId==="nightmare"){
+            challengerAttack = 10;
+        }
+
+        currentAttack += heroAttack;
+        console.log("hero attack: " + heroAttack + " challenger attack: " + challengerAttack + "current Attack: " + currentAttack);
+        heroHealth -= challengerAttack;
+        if (heroHealth<=0) {
+            isHeroDefeated = true;
+            $("#message-box").append("You lost!");
+        }
+        console.log("hero health: " + heroHealth);
+        challengerHealth -= currentAttack;
+        if (challengerHealth<=0 && heroHealth >0) {
+            $(challenger).attr("class", "red-border");
+            $("#challenger-box").empty();
+            $("#enemy-box").append(challenger);
+            isChallengerDefeated = true;
+            console.log(isChallengerDefeated);
+            $("#message-box").append("you defeated your opponent! choose another challenger");
+        } 
+        console.log("challenger health: " + challengerHealth);
+        $("#hero-health").text("Your health: " + heroHealth);
+        $("#challenger-health").text("Challenger health: " + challengerHealth);
+    
+    } 
+
+    function chooseNewChallenger() {
+        isChallengerChosen= false;
+        isChallengerDefeated = false;
+        challenger = "";
+        challengerId = "";
+        challengerHealth = 0;
+        $("#challenger-health, #attack-button").empty();
+    }
 
 
     $(".pony").on("click", function() {
         if (isHeroChosen) {
             return false;
-        }
+        } else {
         $("#enemy-box").append(celestia, discord, twilight, nightmare);
         $("#hero-box").append(this);
         hero = this;
-         isHeroChosen= true; 
-    });
-    $(".pony").on("click", function(){
-        if (isChallengerChosen || this===hero) {
-            return false;
+       // $(this).attr("class", "heroClass");
+        heroId = $(this).attr("id");
+        heroHealth = parseInt($(this).attr("value"));
+        $("#hero-health").append("Your health: " + heroHealth);
+        isHeroChosen= true; 
+       // $(".heroClass").attr("value", "22");
+        console.log(hero);
         }
+    });
+
+    $(".pony").on("click", function chooseChallenger(){
+        if (isChallengerChosen || this===hero || isHeroChosen===false) {
+            return false;
+        } else {
+        $("#message-box").empty();
         $("#challenger-box").append(this);
-        challenger= this;
+        challenger = this;
+        challengerId= $(this).attr("id");
         isChallengerChosen = true;
-    })
-    
+        challengerHealth = parseInt($(this).attr("value"));
+        $("#challenger-health").append("Challenger health: " + challengerHealth);
+        $("#attack-button").append("<button>ATTACK!</button>");
+        };
+    }); 
+
+
+    $("#attack-button").on("click", function() {
+        if (isChallengerDefeated || allEnemiesDefeated || isHeroDefeated) {
+            return false;
+       /* }
+        if (heroHealth<=0) {
+            isHeroDefeated = true;
+            $("#message-box").text("You lost!");
+            return;
+        } else if (challengerHealth<=0) {
+            $(challenger).attr("class", "red-border");
+            $("#challenger-box").empty();
+            $("#enemy-box").append(challenger);
+            isChallengerDefeated = true;
+            $("#message-box").text("you defeated your opponent! choose another challenger");
+            //chooseNewChallenger(); */
+        } else {
+        attackTime();}
+        if (isChallengerDefeated) {
+            chooseNewChallenger();
+        }
+   
+    });
+
+
+/*
+    if (heroHealth<=0) {
+        isHeroDefeated = true;
+        $("#message-box").append("You lost!");
+    }
+     if (challengerHealth<=0 && heroHealth >0) {
+        $(challenger).attr("class", "red-border");
+        $("#challenger-box").empty();
+        $("#enemy-box").append(challenger);
+        isChallengerDefeated = true;
+        $("#message-box").append("you defeated your opponent! choose another challenger");
+    } 
+    if (isChallengerDefeated) {
+        chooseNewChallenger();
+    } */
+
+
 
 
 }); //closes document.ready

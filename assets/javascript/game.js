7d48c05595d4ac990f0478a830bbf1d20b3876bc
Ali <ali@Alis-MacBook-Pro.local>
2018-02-20 15:45:19 -0500

    game works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
 //set all ids to variables
-
-
     var twilight = $("#twilight");
     var celestia = $("#celestia");
     var discord = $("#discord");
     var nightmare = $("#nightmare");
 
+    twilight.attr("attack", 5);
+    celestia.attr("attack", 20);
+    discord.attr("attack", 15);
+    nightmare.attr("attack", 10);
 
+    var ponyArray = [twilight, celestia, discord, nightmare];
   
     isHeroChosen = false;
     isChallengerChosen = false;
@@ -24,52 +27,36 @@ $(document).ready(function(){
     var heroAttack = 0;
     var challengerAttack = 0;
     var currentAttack = 0;
-   // var heroHealthNum = 0; 
-   // var challengerHealthNum = 0; 
+    var wins = 0;
+    var restartBtn = $("<button>Restart</button>");
 
+    function makeRestartBtn() {
+        restartBtn.addClass("btn btn-success");
+        $("#restart").append(restartBtn);
+    }
 
     function attackTime () {
-        if (heroId==="twilight"){
-            heroAttack = 5;
-        } else if (heroId==="celestia"){
-            heroAttack = 20;
-        } else if (heroId==="discord"){
-            heroAttack = 15;
-        } else if (heroId==="nightmare"){
-            heroAttack = 10;
-        } 
-        if (challengerId==="twilight"){
-            challengerAttack = 5;
-        } else if (challengerId==="celestia"){
-            challengerAttack = 20;
-        } else if (challengerId==="discord"){
-            challengerAttack = 15;
-        } else if (challengerId==="nightmare"){
-            challengerAttack = 10;
-        }
-
         currentAttack += heroAttack;
-        console.log("hero attack: " + heroAttack + " challenger attack: " + challengerAttack + "current Attack: " + currentAttack);
         heroHealth -= challengerAttack;
         if (heroHealth<=0) {
             isHeroDefeated = true;
             $("#message-box").append("You lost!");
-        }
-        console.log("hero health: " + heroHealth);
+            makeRestartBtn();
+        };
+
         challengerHealth -= currentAttack;
         if (challengerHealth<=0 && heroHealth >0) {
-            $(challenger).attr("class", "red-border");
+            wins ++;
+            $(challenger).attr("class", "red-border img");
             $("#challenger-box").empty();
             $("#enemy-box").append(challenger);
             isChallengerDefeated = true;
-            console.log(isChallengerDefeated);
-            $("#message-box").append("you defeated your opponent! choose another challenger");
+            $("#message-box").append("You defeated your opponent! Choose another challenger");
         } 
-        console.log("challenger health: " + challengerHealth);
+        $("#attack-nar").text("You attacked for " + currentAttack + " damage! Your opponent attacked and you took " + challengerAttack + " damage!");
         $("#hero-health").text("Your health: " + heroHealth);
         $("#challenger-health").text("Challenger health: " + challengerHealth);
-    
-    } 
+    } ;
 
     function chooseNewChallenger() {
         isChallengerChosen= false;
@@ -77,8 +64,14 @@ $(document).ready(function(){
         challenger = "";
         challengerId = "";
         challengerHealth = 0;
-        $("#challenger-health, #attack-button").empty();
-    }
+        $("#challenger-health, #attack-button, #attack-nar").empty();
+    };
+
+    function youWon() {
+        $("#attack-nar").empty();
+        $("#message-box").text("You have defeated all 3 enemies! You win!");
+        makeRestartBtn();
+    };
 
 
     $(".pony").on("click", function() {
@@ -88,12 +81,11 @@ $(document).ready(function(){
         $("#enemy-box").append(celestia, discord, twilight, nightmare);
         $("#hero-box").append(this);
         hero = this;
-       // $(this).attr("class", "heroClass");
         heroId = $(this).attr("id");
         heroHealth = parseInt($(this).attr("value"));
+        heroAttack = parseInt($(this).attr("attack"));
         $("#hero-health").append("Your health: " + heroHealth);
         isHeroChosen= true; 
-       // $(".heroClass").attr("value", "22");
         console.log(hero);
         }
     });
@@ -101,71 +93,40 @@ $(document).ready(function(){
     $(".pony").on("click", function chooseChallenger(){
         if (isChallengerChosen || this===hero || isHeroChosen===false) {
             return false;
-        } else {
+        } 
         $("#message-box").empty();
         $("#challenger-box").append(this);
         challenger = this;
         challengerId= $(this).attr("id");
         isChallengerChosen = true;
         challengerHealth = parseInt($(this).attr("value"));
+        challengerAttack = parseInt($(this).attr("attack"));
         $("#challenger-health").append("Challenger health: " + challengerHealth);
         $("#attack-button").append("<button>ATTACK!</button>");
-        };
+        
     }); 
 
 
     $("#attack-button").on("click", function() {
         if (isChallengerDefeated || allEnemiesDefeated || isHeroDefeated) {
             return false;
-       /* }
-        if (heroHealth<=0) {
-            isHeroDefeated = true;
-            $("#message-box").text("You lost!");
-            return;
-        } else if (challengerHealth<=0) {
-            $(challenger).attr("class", "red-border");
-            $("#challenger-box").empty();
-            $("#enemy-box").append(challenger);
-            isChallengerDefeated = true;
-            $("#message-box").text("you defeated your opponent! choose another challenger");
-            //chooseNewChallenger(); */
         } else {
-        attackTime();}
+            attackTime();
+        }
         if (isChallengerDefeated) {
             chooseNewChallenger();
+        }
+        if (wins===3) {
+            youWon();
         }
    
     });
 
-
-/*
-    if (heroHealth<=0) {
-        isHeroDefeated = true;
-        $("#message-box").append("You lost!");
-    }
-     if (challengerHealth<=0 && heroHealth >0) {
-        $(challenger).attr("class", "red-border");
-        $("#challenger-box").empty();
-        $("#enemy-box").append(challenger);
-        isChallengerDefeated = true;
-        $("#message-box").append("you defeated your opponent! choose another challenger");
-    } 
-    if (isChallengerDefeated) {
-        chooseNewChallenger();
-    } */
-
-
+    $("#restart").on("click", function () {
+        document.location.reload(true);
+    });
 
 
 }); //closes document.ready
-
-
-//you should use return fase.
-//isFighterChosen = false
-//inside of my normal function first write
-//if (isFighterChosen) {
- //   return false;
-//} return false exits the function.
-//if (!fighter || )
 
 //dustin created an object of methods. had  variable operator is the string
